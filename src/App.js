03cdd8b6196a9e07f0a84605107c6391f033827b
Charles Hodge <hhivt001@gmail.com>
2022-11-07 14:53:48 -0500

    Charles - Create header component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import './App.css';
+import './App.scss';
 import store from './redux/store';
+import Header from './components/header/Header';
 
 const App = () => {
   return (
     <Provider store={store}>
+      <Header />
       <div className="app">
         <h1>HH Reacx Restart1 Website </h1>
       </div>
