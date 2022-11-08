04596e65c7e5fbc93c8e79752e88eeaf9dfcacdf
Charles Hodge <hhivt001@gmail.com>
2022-11-08 17:16:21 -0500

    Add Button Paginate component.

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MainContent.scss';
 import Slideshow from '../slide-show/Slideshow';
+import Paginate from '../paginate/Paginate';
 
 const MainContent = () => {
   const images = [
@@ -17,14 +18,27 @@ const MainContent = () => {
       url: 'https://media.cntraveler.com/photos/611517ea423da55ea76063f6/master/w_4000,h_2667,c_limit/eureka-springs-arkansas-GettyImages-1327311498.jpg'
     }
   ];
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const paginate = (type) => {
+    if (type === 'prev' && currentPage > 1) {
+      setCurrentPage((prev) => prev - 1);
+    } else {
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
+
   return (
     <div className="main-content">
       <Slideshow images={images} auto={true} showArrows={true} />
       <div className="grid-movie-title">
         <div className="movieType">Now Playing</div>
-        <div className="paginate">Paginate</div>
+        <div className="paginate">
+          <Paginate currentPage={currentPage} totalPages={10} paginate={paginate} />
+        </div>
       </div>
-      {/* { display grid component } */}
+      {/* display grid component */}
     </div>
   );
 };
