b3030562d2313e4702966e73153e6b719c9db9af
Charles Hodge <hhivt001@gmail.com>
2022-11-10 17:35:44 -0500

    Charles - Add loaders, enhanced navigations, and button behaviors.

--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -8,6 +8,7 @@ const Spinner = () => {
       <div className="bounce1"></div>
       <div className="bounce2"></div>
       <div className="bounce3"></div>
+      <div className="bounce4"></div>
     </div>
   );
 };
