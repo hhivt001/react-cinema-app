cb728ebe8bdfd3a1c8b3374295f723f154abdde2
Charles Hodge <hhivt001@gmail.com>
2022-11-11 15:39:18 -0500

    Charles - Create initial Details folder and file structure.

--- src/components/content/grid/Grid.js
+++ src/components/content/grid/Grid.js
@@ -7,6 +7,7 @@ import Rating from '../rating/Rating';
 import PropTypes from 'prop-types';
 import { IMAGE_URL } from '../../../services/movies.service';
 import LazyImage from '../../lazy-image/LazyImage';
+import { Link } from 'react-router-dom';
 
 const Grid = (props) => {
   const { list } = props;
@@ -16,6 +17,11 @@ const Grid = (props) => {
     setMovieData(list);
   }, [list]);
 
+  const formatMovieTitle = (title) => {
+    const titleStr = title.toLowerCase();
+    return titleStr.replace(/ /g, '-');
+  };
+
   return (
     <>
       <div className="grid">
@@ -23,12 +29,14 @@ const Grid = (props) => {
           <div key={uuidv4()}>
             <LazyImage className="grid-cell" src={`${IMAGE_URL}${data.poster_path}`} alt="placeholder">
               <div className="grid-read-more">
-                <button className="grid-cell-button">Read More</button>
+              <button className="grid-cell-button">
+                  <Link to={`/${data.id}/${formatMovieTitle(data.title)}/details`}>Read More</Link>
+                </button>
               </div>
               <div className="grid-detail">
                 <span className="grid-detail-title">{data.title}</span>
                 <div className="grid-detail-rating">
-                  <Rating rating={data.vote_average} totalStars={10} />
+                  <Rating rating={data.vote_average} totalStars={5} />
                   &nbsp;&nbsp;
                   <div className="grid-vote-average">{data.vote_average}</div>
                 </div>
